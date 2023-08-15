89683e960cc6f3aba4ee87094012e84a2f1b6e11
JosephMumo <josephmumo25@gmail.com>
2023-08-15 18:26:28 +0300

    feat: Booking foorm functionality

--- Table_Booking/src/components/Booking.jsx
+++ Table_Booking/src/components/Booking.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {PiDotsThreeOutlineFill } from 'react-icons/pi'
+import { myContext } from '../context/AppContext';
+
 
 
 function Booking() {
+    const {handleChange, handleSubmit, info} = useContext(myContext)
     
     return ( 
         <div className='text-center mt-5 p-8 min-h-screen'>
@@ -10,19 +13,19 @@ function Booking() {
             <h1 className='text-3xl font-bold'>Reserve a Table</h1>
             <br />
             <div className='p-5'>
-                <form className='flex flex-col justify-center space-y-10 p-10 md:p-20'>
+                <form className='flex flex-col justify-center space-y-10 p-10 md:p-20' onSubmit={handleSubmit}>
                     <div className='flex flex-col md:flex-row justify-between min-h-48 items-center'>
                         <section className='p-2 flex flex-col space-y-10'>
                             <label htmlFor='date' className='font-bold'>Date</label>
-                            <input type='date' id='date' className='bg-slate-50' />
+                            <input type='date' id='date' name='date' value={info.date} className='bg-slate-50' onChange={handleChange} />
                         </section>
                         <section className='p-2 flex flex-col space-y-10'>
                             <label htmlFor='time' className='font-bold'>Time</label>
-                            <input type='time' id='time' className='bg-slate-50' />
+                            <input type='time' id='time' name='time' value={info.time} className='bg-slate-50' onChange={handleChange} />
                         </section>
                         <section className='p-2 flex flex-col space-y-10'>
                             <label htmlFor='people' className='font-bold'>People</label>
-                            <input type='number' id='people' placeholder='No. people' className='bg-slate-50' />
+                            <input type='number' id='people' name='people' value={info.people} placeholder='No. people' className='bg-slate-50' onChange={handleChange} />
                         </section>
                     </div>
                     <div className='w-full flex justify-center mb-10'>
--- Table_Booking/src/context/AppContext.jsx
+++ Table_Booking/src/context/AppContext.jsx
@@ -1,14 +1,43 @@
-import React, { createContext, useEffect } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import AOS from 'aos'
 
-const myContext = createContext()
+export const myContext = createContext()
 
 function AppContextProvider({children}) {
     useEffect(() => {
         AOS.init( {duration: 2000})
     }, [])
+    
+    const [info, setInfo] = useState({
+        date: '',
+        time:'',
+        people: ''
+    })
+    const [book, setBook] = useState([])
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setInfo(prev => {
+            return(
+                { ...prev,
+                [name]: value
+            }
+            )
+        })
+        console.log(info)
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        setBook(prev => {
+            return([...prev, book])
+        })
+        console.log(book)
+        setInfo({
+            date:"",time:"",people:""
+        })
+    }
     return(
-        <myContext.Provider value={{}}>
+        <myContext.Provider value={{handleChange, handleSubmit, info, book, setBook, setInfo}}>
             {children}
         </myContext.Provider>
     )
