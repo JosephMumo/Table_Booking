bce46ea159cd73d33fbf9967a7a2e547e66aaac9
JosephMumo <josephmumo25@gmail.com>
2023-08-21 14:24:55 +0300

    pdf viewer implementation

--- Table_Booking/src/components/Menu.jsx
+++ Table_Booking/src/components/Menu.jsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Footer from './Footer';
-import { Document, Page } from 'react-pdf';
+import { Document, PDFViewer } from '@react-pdf/renderer';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import pdfFile from '../sample.pdf'
 
 
 function Menu() {
-    const [numPages, setNumPages] = useState(null)
-    const [pageNumber, setPageNumber] = useState(1)
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages)
+    const styles = {
+        left: 0
     }
+    
     return (  
         <>
-            <div className='min-h-screen'>
+            <div className='min-h-screen w-full'>
                 <h1 className="text-center">Menu</h1>
-                <Document
-                    file={pdfFile}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
+                <PDFViewer>
+                    <Document file={pdfFile} style={{styles}} className='w-full'></Document>
+                </PDFViewer>
             </div>
             <Footer />
         </>
