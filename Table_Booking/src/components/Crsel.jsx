616e6e2ff49a70b0d96ee668061d61f8570c2ed5
JosephMumo <josephmumo25@gmail.com>
2023-08-23 16:24:24 +0300

    debug: Carousel alignment

--- Table_Booking/src/components/Crsel.jsx
+++ Table_Booking/src/components/Crsel.jsx
@@ -9,16 +9,18 @@ const Crsel = () => {
     return (
         <div>
             <Carousel className='grid place-items-center'>
-                <div className='mt-5 md:p-24 relative'>
+                <div className='mt-3 md:px-24 relative'>
                     <img src={bdy} alt='1' />
-                    <h3 className='text-2xl font-semibold'>Private Parties</h3>
-                    <br />
-                    <h1 className='text-3xl font-bold border-b border-primary'>$289</h1>
-                    <br />
-                    <p>Indulge in exclusive gatherings at our restaurant. Elevate your private parties with exquisite cuisine, impeccable service, and a charming ambiance. Unforgettable memories await.</p>
+                    <div>
+                        <h3 className='text-2xl font-semibold'>Private Parties</h3>
+                        <br />
+                        <h1 className='text-3xl font-bold border-b border-primary'>$289</h1>
+                        <br />
+                        <p>Indulge in exclusive gatherings at our restaurant. Elevate your private parties with exquisite cuisine, impeccable service, and a charming ambiance. Unforgettable memories await.</p>
+                    </div>
                             
                 </div>
-                <div className=' mt-5 md:p-24'>
+                <div className=' mt-3 md:px-24'>
                     <img src={cdy} alt="2" />
                     <h3 className='text-2xl font-semibold'>Birthday Parties</h3>
                     <br />
@@ -26,7 +28,7 @@ const Crsel = () => {
                     <br />
                     <p>Celebrate joyous milestones with us! Host remarkable birthday parties, relishing delectable dishes, lively atmosphere, and delightful moments that linger.</p>
                 </div>
-                <div className=' mt-5 md:p-24'>
+                <div className=' mt-3 md:px-24'>
                     <img src={pdy} alt="3" />
                     <h3 className='text-2xl font-semibold'>Custom Parties</h3>
                     <br />
